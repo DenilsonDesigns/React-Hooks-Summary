ff7775ec7470f07740763f7d4c0a72808dab3cae
Daniel <daniellanguilller@gmail.com>
2021-06-12 18:33:43 +1000

    working with useEffect

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,16 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import IngredientForm from "./IngredientForm/IngredientForm";
 import IngredientList from "./IngredientList/IngredientList";
 import Search from "./Search/Search";
 
+const BASE_URL =
+  "https://react-hooks-summary-add5d-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
+  // // when useEffect is used like this (with empty array as second arg.) /
+  // // then it becomes like "componentDidMount()"
+  // useEffect(() => {
+  //   // useEffect runs AFTER every single render cycle if no dependency passed\
+  //   // as second argument.
+  //   fetch(`${BASE_URL}/ingredients.json`)
+  //     .then((res) => res.json())
+  //     .then((resData) => {
+  //       const loadedIng = [];
+  //       for (const key in resData) {
+  //         loadedIng.push({
+  //           id: key,
+  //           title: resData[key].title,
+  //           amount: resData[key].amount,
+  //         });
+  //       }
+  //       setIngredients(loadedIng);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    console.log("Rendering Ingredients", ingredients);
+  }, [ingredients]);
+
   const addIngredientHandler = (ingredient) => {
-    setIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(`${BASE_URL}/ingredients.json`, {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((resData) => {
+        setIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: resData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (ingredientId) => {
@@ -19,12 +58,20 @@ const Ingredients = () => {
     );
   };
 
+  // using useCallback like this means it survices re-render cycles. /
+  // meaning that it wont have "changed", and then wont cause /
+  // Search.jsx to re-render if it has onLoadIngredients as a /
+  // dependency in a useEffect.
+  const filterIngHandler = useCallback((filteredIng) => {
+    setIngredients(filteredIng);
+  }, []);
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filterIngHandler} />
         <IngredientList
           ingredients={ingredients}
           onRemoveItem={removeIngredientHandler}
--- src/components/Ingredients/Search/Search.jsx
+++ src/components/Ingredients/Search/Search.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Card from "../../UI/Card";
 import "./Search.css";
 
+const BASE_URL =
+  "https://react-hooks-summary-add5d-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
 const Search = React.memo((props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState("");
+
+  useEffect(() => {
+    const query =
+      enteredFilter.length === 0
+        ? ""
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    fetch(`${BASE_URL}/ingredients.json${query}`)
+      .then((res) => res.json())
+      .then((resData) => {
+        const loadedIng = [];
+        for (const key in resData) {
+          loadedIng.push({
+            id: key,
+            title: resData[key].title,
+            amount: resData[key].amount,
+          });
+        }
+        onLoadIngredients(loadedIng);
+      });
+  }, [enteredFilter, onLoadIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredFilter}
+            onChange={(e) => setEnteredFilter(e.target.value)}
+          />
         </div>
       </Card>
     </section>
