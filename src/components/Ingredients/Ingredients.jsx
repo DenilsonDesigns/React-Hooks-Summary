718eb1da404f3343b51bb18af6a58c1498292172
Daniel <daniellanguilller@gmail.com>
2021-06-14 20:21:39 +1000

    useReducer and useContext egs

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,14 +1,51 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useReducer, useCallback } from "react";
 import IngredientForm from "./IngredientForm/IngredientForm";
 import IngredientList from "./IngredientList/IngredientList";
 import Search from "./Search/Search";
+import ErrorModal from "../UI/ErrorModal";
 
 const BASE_URL =
   "https://react-hooks-summary-add5d-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
-const Ingredients = () => {
-  const [ingredients, setIngredients] = useState([]);
+const ingredientReducer = (currentIng, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIng, action.ingredient];
+    case "DELETE":
+      return currentIng.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error("Should not get to default switch.");
+  }
+};
 
+const httpReducer = (currHttpState, action) => {
+  switch (action.type) {
+    case "SEND":
+      return { loading: true, error: null };
+    case "RESPONSE":
+      return { ...currHttpState, loading: false };
+    case "ERROR":
+      return { loading: false, error: action.errorMsg };
+    case "CLEAR":
+      return { ...currHttpState, error: null };
+    default:
+      throw new Error("Should not be reached!");
+  }
+};
+
+const Ingredients = () => {
+  // const [ingredients, setIngredients] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
+  const [ingredients, dispatchIngredients] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHttpState] = useReducer(httpReducer, {
+    loading: false,
+    error: null,
+  });
+
+  // *** Note ***
   // // when useEffect is used like this (with empty array as second arg.) /
   // // then it becomes like "componentDidMount()"
   // useEffect(() => {
@@ -28,12 +65,11 @@ const Ingredients = () => {
   //       setIngredients(loadedIng);
   //     });
   // }, []);
-
-  useEffect(() => {
-    console.log("Rendering Ingredients", ingredients);
-  }, [ingredients]);
+  // ***
 
   const addIngredientHandler = (ingredient) => {
+    // setIsLoading(true);
+    dispatchHttpState({ type: "SEND" });
     fetch(`${BASE_URL}/ingredients.json`, {
       method: "POST",
       body: JSON.stringify(ingredient),
@@ -42,37 +78,66 @@ const Ingredients = () => {
       },
     })
       .then((res) => {
+        // setIsLoading(false);
+        dispatchHttpState({ type: "RESPONSE" });
         return res.json();
       })
       .then((resData) => {
-        setIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: resData.name, ...ingredient },
-        ]);
+        // setIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   { id: resData.name, ...ingredient },
+        // ]);
+        dispatchIngredients({
+          type: "ADD",
+          ingredient: { id: resData.name, ...ingredient },
+        });
       });
   };
 
   const removeIngredientHandler = (ingredientId) => {
+    // setIsLoading(true);
+    dispatchHttpState({ type: "SEND" });
     fetch(`${BASE_URL}/ingredients/${ingredientId}.json`, {
       method: "DELETE",
-    }).then(() => {
-      setIngredients((prevIngredients) =>
-        prevIngredients.filter((ing) => ing.id !== ingredientId)
-      );
-    });
+    })
+      .then(() => {
+        // setIsLoading(false);
+        // setIngredients((prevIngredients) =>
+        //   prevIngredients.filter((ing) => ing.id !== ingredientId)
+        // );
+        dispatchHttpState({ type: "RESPONSE" });
+        dispatchIngredients({ type: "DELETE", id: ingredientId });
+      })
+      .catch((err) => {
+        // setError("Something went wrong!", err);
+        // setIsLoading(false);
+        dispatchHttpState({ type: "ERROR", errorMsg: err.message });
+      });
   };
 
-  // using useCallback like this means it survices re-render cycles. /
+  // using useCallback like this means it survives re-render cycles. /
   // meaning that it wont have "changed", and then wont cause /
   // Search.jsx to re-render if it has onLoadIngredients as a /
   // dependency in a useEffect.
   const filterIngHandler = useCallback((filteredIng) => {
-    setIngredients(filteredIng);
+    // setIngredients(filteredIng);
+    dispatchIngredients({ type: "SET", ingredients: filteredIng });
   }, []);
+
+  const clearError = () => {
+    // setError(null);
+    dispatchHttpState({ type: "CLEAR" });
+  };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {httpState.error && (
+        <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>
+      )}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={httpState.loading}
+      />
 
       <section>
         <Search onLoadIngredients={filterIngHandler} />
